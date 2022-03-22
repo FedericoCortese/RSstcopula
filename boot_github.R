8bda623ca73d675358f4ab89f93fd5804f970431
Federico P. Cortese <67327161+FedericoCortese@users.noreply.github.com>
2022-03-22 15:36:33 +0100

    Add files via upload

---
+++ boot_github.R
@@ -0,0 +1,235 @@
+
+# semiparam__2-states -----------------------------------------------------
+library(bootstrap)
+#theta <- function(x){mean(x)} 
+#results = bootstrap(testFile,100,theta) 
+library(boot)
+library(parallel)
+library(rugarch)
+library(fGarch)
+library(skewt)
+
+
+semiparametric_RScop_boot <- function(x) {
+  #marginal parameters
+  R=apply(x,2,rank)/(dim(x)[1]+1)
+  
+  #RS copula param
+  #sourceCpp("RSest_trede_C.cpp")
+  est_rsc=Est_comp_C(R,2)
+  Qvec=as.vector(est_rsc$Q)
+  Rvec=as.vector(apply(est_rsc$R,3,function(x)x[upper.tri(x)]))
+  #Rvec=as.vector(est_rsc$R)
+  cop.pars=c(est_rsc$init,Qvec,Rvec,est_rsc$nu)
+  return(cop.pars)
+}
+
+l1=round(dim(x)[1]^(2/3))
+nboot=1000
+library(bootstrap)
+library(boot)
+startsp2=Sys.time()
+param_boot_2sp <- tsboot(x, semiparametric_RScop_boot, R = nboot, l = l1, sim = "geom",
+                         parallel = "multicore", ncpus = detectCores(), n.sim=dim(x)[1])
+
+endsp2=Sys.time()
+endsp2-startsp2
+
+# semiparam__3-states -----------------------------------------------------
+
+library(bootstrap)
+#theta <- function(x){mean(x)} 
+#results = bootstrap(testFile,100,theta) 
+library(boot)
+library(parallel)
+library(rugarch)
+library(fGarch)
+library(skewt)
+
+
+semiparametric_RScop_boot <- function(x) {
+  #marginal parameters
+  R=apply(x,2,rank)/(dim(x)[1]+1)
+  
+  #RS copula param
+  #sourceCpp("RSest_trede_C.cpp")
+  est_rsc=Est_comp_C(R,3)
+  Qvec=as.vector(est_rsc$Q)
+  Rvec=as.vector(apply(est_rsc$R,3,function(x)x[upper.tri(x)]))
+  #Rvec=as.vector(est_rsc$R)
+  cop.pars=c(est_rsc$init,Qvec,Rvec,est_rsc$nu)
+  return(cop.pars)
+}
+
+l1=round(dim(x)[1]^(2/3))
+nboot=1000
+library(bootstrap)
+library(boot)
+startsp2=Sys.time()
+param_boot_3sp <- tsboot(x, semiparametric_RScop_boot, R = nboot, l = l1, sim = "geom",
+                         parallel = "multicore", ncpus = detectCores(), n.sim=dim(x)[1])
+
+endsp2=Sys.time()
+endsp2-startsp2
+
+
+# param__2-states ---------------------------------------------------------
+#x=as.matrix(data[,7:11])
+parametric_RScop_boot <- function(U) {
+  #marginal parameters
+  btc = x[,1]
+  eth=x[,2]
+  xrp=x[,3]
+  ltc=x[,4]
+  bch=x[,5]
+
+  model = "sGARCH"
+  spec = ugarchspec(
+    variance.model = list(model = model, garchOrder = c(1, 1)),
+    mean.model = list(armaOrder = c(1, 1), include.mean = FALSE) ,
+    distribution.model = "sged"
+  )
+
+  fit.btc = ugarchfit(spec, btc)
+  fit.eth = ugarchfit(spec, eth)
+  fit.ltc = ugarchfit(spec, ltc)
+  fit.xrp = ugarchfit(spec, xrp)
+  fit.bch = ugarchfit(spec, bch)
+
+  marg.pars=c(fit.btc@fit$coef,
+              fit.eth@fit$coef,
+              fit.ltc@fit$coef,
+              fit.xrp@fit$coef,
+              fit.bch@fit$coef)
+
+  nu.b = fit.btc@fit$coef["shape"]
+  nu.e = fit.eth@fit$coef["shape"]
+  nu.l = fit.ltc@fit$coef["shape"]
+  nu.x = fit.xrp@fit$coef["shape"]
+  nu.h = fit.bch@fit$coef["shape"]
+
+  sk.b = fit.btc@fit$coef["skew"]
+  sk.e = fit.eth@fit$coef["skew"]
+  sk.l = fit.ltc@fit$coef["skew"]
+  sk.x = fit.xrp@fit$coef["skew"]
+  sk.h = fit.bch@fit$coef["skew"]
+
+  #standardize residuals
+  st.res.b = fit.btc@fit$residuals / fit.btc@fit$sigma
+  st.res.e = fit.eth@fit$residuals / fit.eth@fit$sigma
+  st.res.l = fit.ltc@fit$residuals / fit.ltc@fit$sigma
+  st.res.x = fit.xrp@fit$residuals / fit.xrp@fit$sigma
+  st.res.h = fit.bch@fit$residuals / fit.bch@fit$sigma
+
+  #get integral transforms
+  U1 = psged(st.res.b, nu = nu.b,xi=sk.b)
+  U2 = psged(st.res.e, nu = nu.e,xi=sk.e)
+  U3 = psged(st.res.l, nu = nu.l,xi=sk.l)
+  U4 = psged(st.res.x, nu = nu.x,xi=sk.x)
+  U5 = psged(st.res.h, nu = nu.h,xi=sk.h)
+
+  UU=cbind(U1,U2,U3,U4,U5)
+  U=apply(UU,2,rank)/(dim(UU)[1]+1)
+  
+  #RS copula param
+  #sourceCpp("RSest_trede_C.cpp")
+  est_rsc=Est_comp_C(U,2)
+  Qvec=as.vector(est_rsc$Q)
+  Rvec=as.vector(apply(est_rsc$R,3,function(x)x[upper.tri(x)]))
+  #Rvec=as.vector(est_rsc$R)
+  cop.pars=c(est_rsc$init,Qvec,Rvec,est_rsc$nu)
+  return(c(cop.pars,marg.pars))
+}
+
+l1=round(dim(U)[1]^(2/3))
+nboot=1000
+library(bootstrap)
+library(boot)
+startp2=Sys.time()
+param_boot_2p <- tsboot(U, parametric_RScop_boot, R = nboot, l = l1, sim = "geom",
+                        parallel = "multicore", ncpus = detectCores(), n.sim=dim(x)[1])
+
+endp2=Sys.time()
+endp2-startp2
+
+
+# param__3-states ---------------------------------------------------------
+
+parametric_RScop_boot <- function(U) {
+  #marginal parameters
+  btc = x[,1]
+  eth=x[,2]
+  xrp=x[,3]
+  ltc=x[,4]
+  bch=x[,5]
+  
+  model = "sGARCH"
+  spec = ugarchspec(
+    variance.model = list(model = model, garchOrder = c(1, 1)),
+    mean.model = list(armaOrder = c(1, 1), include.mean = FALSE) ,
+    distribution.model = "sged"
+  )
+  
+  fit.btc = ugarchfit(spec, btc)
+  fit.eth = ugarchfit(spec, eth)
+  fit.ltc = ugarchfit(spec, ltc)
+  fit.xrp = ugarchfit(spec, xrp)
+  fit.bch = ugarchfit(spec, bch)
+  
+  marg.pars=c(fit.btc@fit$coef,
+              fit.eth@fit$coef,
+              fit.ltc@fit$coef,
+              fit.xrp@fit$coef,
+              fit.bch@fit$coef)
+  
+  nu.b = fit.btc@fit$coef["shape"]
+  nu.e = fit.eth@fit$coef["shape"]
+  nu.l = fit.ltc@fit$coef["shape"]
+  nu.x = fit.xrp@fit$coef["shape"]
+  nu.h = fit.bch@fit$coef["shape"]
+  
+  sk.b = fit.btc@fit$coef["skew"]
+  sk.e = fit.eth@fit$coef["skew"]
+  sk.l = fit.ltc@fit$coef["skew"]
+  sk.x = fit.xrp@fit$coef["skew"]
+  sk.h = fit.bch@fit$coef["skew"]
+  
+  #standardize residuals
+  st.res.b = fit.btc@fit$residuals / fit.btc@fit$sigma
+  st.res.e = fit.eth@fit$residuals / fit.eth@fit$sigma
+  st.res.l = fit.ltc@fit$residuals / fit.ltc@fit$sigma
+  st.res.x = fit.xrp@fit$residuals / fit.xrp@fit$sigma
+  st.res.h = fit.bch@fit$residuals / fit.bch@fit$sigma
+  
+  #get integral transforms
+  U1 = psged(st.res.b, nu = nu.b,xi=sk.b)
+  U2 = psged(st.res.e, nu = nu.e,xi=sk.e)
+  U3 = psged(st.res.l, nu = nu.l,xi=sk.l)
+  U4 = psged(st.res.x, nu = nu.x,xi=sk.x)
+  U5 = psged(st.res.h, nu = nu.h,xi=sk.h)
+  
+  UU=cbind(U1,U2,U3,U4,U5)
+  U=apply(UU,2,rank)/(dim(UU)[1]+1)
+  
+  #RS copula param
+  #sourceCpp("RSest_trede_C.cpp")
+  est_rsc=Est_comp_C(U,3)
+  Qvec=as.vector(est_rsc$Q)
+  Rvec=as.vector(apply(est_rsc$R,3,function(x)x[upper.tri(x)]))
+  #Rvec=as.vector(est_rsc$R)
+  cop.pars=c(est_rsc$init,Qvec,Rvec,est_rsc$nu)
+  return(c(cop.pars,marg.pars))
+}
+
+l1=round(dim(U)[1]^(2/3))
+nboot=1000
+library(bootstrap)
+library(boot)
+startp2=Sys.time()
+param_boot_3p <- tsboot(U, parametric_RScop_boot, R = nboot, l = l1, sim = "geom",
+                        parallel = "multicore", ncpus = detectCores(), n.sim=dim(x)[1])
+
+endp2=Sys.time()
+endp2-startp2
+
+
---
+++ sim_github.R
@@ -0,0 +1,192 @@
+# first scenario ----------------------------------------------------------
+Qtrue2=matrix(c(.8,.2,
+                .2,.8),2,2,byrow=T)
+nutrue2=c(5,15)
+R1=matrix(c(1,0.9,0.9,0.9,0.9,
+            0.9,1,0.9,0.9,0.9,
+            0.9,0.9,1,0.9,0.9,
+            0.9,0.9,0.9,1,0.9,
+            0.9,0.9,0.9,0.9,1),5,5,byrow = T)
+R2=matrix(c(1.000,  0.2 , 0 , 0.1 ,0,  
+            0.2,  1.000, 0 , 0.2,0.2,
+            0 , 0 , 1.000  ,0.1  ,0.2,
+            0.1 , 0.2  ,0.1, 1.000,  0,
+            0 , 0.2 , 0.2 , 0 , 1.000),5,5,byrow=T)
+Rtrue2=array(0,dim=c(5,5,2))
+Rtrue2[,,1]=R1;
+Rtrue2[,,2]=R2;
+
+
+# second scenario ---------------------------------------------------------
+R1=matrix(c(1,.9,.7,.8,.8,
+            .9, 1,.75,.9,.8,
+            .7, .75, 1,.7,.8,
+            .8,.9,.7,1,.8,
+            .8,.8,.8,.8,1),5,5)
+
+R2=matrix(c(1,.5,.3,.5,.4,
+            .5, 1,.4,.4,.5,
+            .3, .4, 1,.4,.5,
+            .5,.4,.4,1,.3,
+            .4,.5,.5,.3,1),5,5)
+
+R3=matrix(c(1,.1,.15,.05,.05,
+            .1,1,-.1,.1,-0.05,
+            .15,-.1,1,.05,.1,
+            .05,.1,.05,1,-.01,
+            .05,-.05,.1,-.01,1),5,5)
+
+Rtrue=array(0,dim=c(5,5,3))
+Rtrue[,,1]=R1;
+Rtrue[,,2]=R2;
+Rtrue[,,3]=R3;
+
+nutrue=c(3,6,10)
+
+Qtrue=matrix(c(.7,.2,.1,
+               .3,.6,.1,
+               .1,.1,.8),3,3,byrow=T)
+inittrue=rep(1/3,3)
+
+stime_trede <- function(seed) {
+  Us=copSim(1500,5,Rtrue,nutrue,Qtrue,inittrue,seed)
+  
+  est = Est_comp_C(Us, reg=3, maxiter = 1000,eps=1e-08, 
+                   ninit = 1,h=0)
+  return(est)
+}
+
+D=1000
+start = Sys.time()
+sim_est_35 <- parallel::mclapply(X=1:D, stime_trede, mc.cores = parallel::detectCores())
+end = Sys.time()
+elapsed = end - start
+
+
+# third scenario ----------------------------------------------------------
+##varying T
+Qtrue2=matrix(c(.8,.2,
+                .2,.8),2,2,byrow=T)
+nutrue2=c(5,15)
+R1=matrix(c(1,0.9,0.9,0.9,0.9,
+            0.9,1,0.9,0.9,0.9,
+            0.9,0.9,1,0.9,0.9,
+            0.9,0.9,0.9,1,0.9,
+            0.9,0.9,0.9,0.9,1),5,5,byrow = T)
+R2=matrix(c(1.000,  0.2 , 0 , 0.1 ,0,  
+            0.2,  1.000, 0 , 0.2,0.2,
+            0 , 0 , 1.000  ,0.1  ,0.2,
+            0.1 , 0.2  ,0.1, 1.000,  0,
+            0 , 0.2 , 0.2 , 0 , 1.000),5,5,byrow=T)
+Rtrue2=array(0,dim=c(5,5,2))
+Rtrue2[,,1]=R1;
+Rtrue2[,,2]=R2;
+
+inittrue2=c(1/2,1/2)
+
+Qtrue3=matrix(c(.7,.2,.1,
+                .2,.7,.1,
+                .1,.2,.7),3,3,byrow=T)
+nutrue3=c(3,7,15)
+R1=matrix(c(1,0.9,0.9,0.9,0.9,
+            0.9,1,0.9,0.9,0.9,
+            0.9,0.9,1,0.9,0.9,
+            0.9,0.9,0.9,1,0.9,
+            0.9,0.9,0.9,0.9,1),5,5,byrow = T)
+R2=matrix(c(1.000, 0.4, 0.5, 0.5, 0.4,
+            0.4, 1.000, 0.4, 0.5, 0.4,
+            0.5,0.4 ,1.000, 0.5, 0.5,
+            0.5, 0.5 ,0.5 ,1.000 ,0.4,
+            0.4, 0.4, 0.5 ,0.4, 1.000),5,5,byrow=T)
+
+R3=matrix(c(1.000,  0.2 , 0 , 0.1 ,0,  
+            0.2,  1.000, 0 , 0.2,0.2,
+            0 , 0 , 1.000  ,0.1  ,0.2,
+            0.1 , 0.2  ,0.1, 1.000,  0,
+            0 , 0.2 , 0.2 , 0 , 1.000),5,5,byrow=T)
+
+Rtrue3=array(0,dim=c(5,5,3))
+Rtrue3[,,1]=R1;
+Rtrue3[,,2]=R2;
+Rtrue3[,,3]=R3;
+
+inittrue3=c(1/3,1/3,1/3)
+
+n_sim <- seq(500, 2000, by=500)
+n_seed <- 1:1000
+
+simulazione <- expand.grid(n_seed=n_seed, n_sim=n_sim)
+
+
+stime_trede <- function(seed,n_sim,reg,Rtrue,nutrue,Qtrue,inittrue) {
+  Us=copSim(n_sim,d=5,Rtrue,nutrue,Qtrue,inittrue,seed)
+  
+  est = Est_comp_C(Us, reg=reg, maxiter = 1000,eps=1e-08, 
+                   ninit = 1,h=0)
+  return(est)
+}
+
+
+start2 = Sys.time()
+sim_est_n2 <- parallel::mclapply(X=1:nrow(simulazione), 
+                                 function(x) stime_trede(seed=simulazione[x,]$n_seed,
+                                                         n_sim=simulazione[x,]$n_sim,reg=2,
+                                                         Rtrue=Rtrue2,nutrue=nutrue2,
+                                                         Qtrue=Qtrue2,inittrue = inittrue2) ,
+                                 mc.cores = parallel::detectCores())
+end2 = Sys.time()
+elapsed2 = end2 - start2
+
+start3= Sys.time()
+sim_est_n3 <- parallel::mclapply(X=1:nrow(simulazione), 
+                                 function(x) stime_trede(seed=simulazione[x,]$n_seed,
+                                                         n_sim=simulazione[x,]$n_sim,reg=3,
+                                                         Rtrue=Rtrue3,nutrue=nutrue3,
+                                                         Qtrue=Qtrue3,inittrue = inittrue3) ,
+                                 mc.cores = parallel::detectCores())
+end3= Sys.time()
+elapsed3 = end3 - start3
+
+##varying r
+Qtrue=matrix(c(.8,.2,
+               .2,.8),2,2,byrow=T)
+nutrue=c(5,15)
+inittrue=c(1/2,1/2)
+
+n_seed <- 1:1000
+n_d=c(2,5,10)
+
+simulazione <- expand.grid(n_seed=n_seed, n_d=n_d)
+
+
+stime_trede <- function(seed,d,nutrue,Qtrue,inittrue) {
+  R1=matrix(.9,d,d)
+  diag(R1)=1
+  R2=matrix(.1,d,d)
+  diag(R2)=1
+  Rtrue=array(0,dim=c(d,d,2))
+  Rtrue[,,1]=R1
+  Rtrue[,,2]=R2
+  Us=copSim(1000,d=d,Rtrue,nutrue,Qtrue,inittrue,seed)
+  
+  est = Est_comp_C(Us, reg=2, maxiter = 1000,eps=1e-08, 
+                   ninit = 1,h=0)
+  return(est)
+}
+
+
+startd = Sys.time()
+sim_est_d <- parallel::mclapply(X=1:nrow(simulazione), 
+                                function(x) stime_trede(seed=simulazione[x,]$n_seed,
+                                                        d=simulazione[x,]$n_d,
+                                                        nutrue=nutrue,
+                                                        Qtrue=Qtrue,inittrue = inittrue) ,
+                                mc.cores = parallel::detectCores())
+endd = Sys.time()
+elapsedd = endd - startd
+
+
+
+
+
+
